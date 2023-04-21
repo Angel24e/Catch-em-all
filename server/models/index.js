d6ec671486ca4086647dcc8a430792bd58a9d496
angel Escamilla <angel4esc@gmail.com>
2023-04-20 19:16:39 -0700

    Made all the boiler plate code and comments on what needs to be changed

---
+++ server/models/index.js
@@ -0,0 +1,9 @@
+const User = require('./User');
+// change product to pokemon
+const Product = require('./Product');
+// change category to type
+const Category = require('./Type');
+// change order to adoption
+const Order = require('./Adoption');
+
+module.exports = { User, Product, Category, Order };
