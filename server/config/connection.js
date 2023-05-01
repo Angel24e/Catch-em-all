36130b58399c1c1f89da3441e4c410231dfc0a7c
angel Escamilla <angel4esc@gmail.com>
2023-05-01 16:58:11 -0700

    done

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:admin@cluster0.yot6dod.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/catch-em-all', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
